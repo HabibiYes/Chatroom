d742e68c610980732a59510e37eab21f7ff9c0e1
HabibiYes <calebfparr@gmail.com>
2025-04-02 11:18:46 -0500

    Message box created with typing.

--- client.py
+++ client.py
@@ -18,6 +18,18 @@ bg_color = Color(128, 128, 128)
 # Font
 font = pg.font.Font('font.ttf', 32)
 
+# Messages
+chat = []
+max_lines = 13
+
+# Chat box
+send = False
+typed_message = ''
+valid_keys = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z',
+              'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z',
+              '`','~','!','@','#','$','%','^','&','*','(',')','-','_','=','+','[',']','{','}','\\','|',';',':','\'','"',
+              ',','.','<','>','/','?',' ']
+
 # Client setup
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 port = 50903
@@ -41,18 +53,21 @@ print(s.recv(1024).decode())
 running = True
 
 def send_messages():
-    global running, time_since_last_interaction, chat
+    global running, time_since_last_interaction, chat, send, typed_message
     while running:
-        message = input('')
-        if message.lower().strip() == 'close':
+        while not send:
+            pass
+        send = False
+        if typed_message.lower().strip() == 'close':
             running = False
             break
         
         try:
-            s.send(f'{user}: {message}'.encode())
-            chat.append(f'{user}: {message}' + '\n')
+            s.send(f'{user}: {typed_message}'.encode())
+            chat.append(f'{user}: {typed_message}' + '\n')
             chat = chat[-10:]
             time_since_last_interaction = time.time()
+            typed_message = ''
         except socket.timeout:
             pass
         except:
@@ -89,11 +104,8 @@ receive_messages_thread = threading.Thread(target=receive_messages)
 send_messages_thread.start()
 receive_messages_thread.start()
 
-chat = []
-max_lines = 13
-
 def main_loop():
-    global running
+    global running, typed_message, send
     while running:
         # Disconnect client if no interaction happens in 1 hour
         if time.time() - time_since_last_interaction > 3600:
@@ -108,22 +120,48 @@ def main_loop():
                 return
 
             if event.type == pg.KEYDOWN:
+                # Quit if escape key pressed
                 if event.key == pg.K_ESCAPE:
                     pg.quit()
                     running = False
                     return
+                
+                # Check unicode validity, then add it to the typed message
+                if event.unicode in valid_keys:
+                    typed_message += event.unicode
+                
+                # Delete the newest character in the typed message
+                if event.key == pg.K_BACKSPACE:
+                    typed_message = typed_message[:len(typed_message) - 1]
 
+                # Send the message
+                if event.key == pg.K_RETURN:
+                    send = True
+                
+                
+        display.fill(bg_color)
+
+        # Messages
         chat_objs = []
         for msg in chat:
             surf:pg.Surface = font.render(msg, False, Color(255, 255, 255))
             chat_objs.insert(0, surf)
-        
-        display.fill(bg_color)
 
         y = display.get_height()
         for surf in chat_objs:
             y -= surf.get_height()
             display.blit(surf, (display.get_width()/2 - surf.get_width()/2, y, surf.get_width(), surf.get_height()))
+
+
+        # Message box
+        x = 25
+        y = display.get_height() - 100
+        width = display.get_width() - 50
+        height = 75
+        rect = pg.Rect(x, y, width, height)
+        pg.draw.rect(display, Color(175, 175, 175), rect)
+        surf = font.render(typed_message, False, Color(30, 30, 30))
+        display.blit(surf, rect)
 
         pg.display.update()
 
